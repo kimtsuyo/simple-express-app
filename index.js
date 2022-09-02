4484b2a2948f82a20d8c1d0ccc4f59e93b95edea
Tsuyoshi Kimura <kimtsuyo@amazon.co.jp>
2022-09-02 20:37:29 +0900

    1st

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path')
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get('/about', function (req, res) {
+    res.send('About')
+  })
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
